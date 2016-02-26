3769b9b90b77a610af4cc6d414f9637066d017d2
Ilkka Seppälä <ilkka.seppala@gofore.com>
2016-02-26 09:50:30 +0200

    Cd to correct directory

--- xroad-catalog-lister/build_rpm.sh
+++ xroad-catalog-lister/build_rpm.sh
@@ -5,5 +5,5 @@ ls -l
 cd workspace
 pwd
 ls -l
-cd packages/xroad-catalog-lister/redhat
+cd workspace/xroad-catalog-lister/packages/xroad-catalog-lister/redhat
 rpmbuild --define "_topdir `pwd`" -ba SPECS/xroad-catalog-lister.spec
