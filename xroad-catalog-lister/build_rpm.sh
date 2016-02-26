40106b0a0a2b36ea9a6b34120381daf0c1903ad0
Ilkka Seppälä <ilkka.seppala@gofore.com>
2016-02-26 09:51:45 +0200

    Fix cd

--- xroad-catalog-lister/build_rpm.sh
+++ xroad-catalog-lister/build_rpm.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-pwd
-ls -l
-cd workspace
-pwd
-ls -l
 cd workspace/xroad-catalog-lister/packages/xroad-catalog-lister/redhat
 rpmbuild --define "_topdir `pwd`" -ba SPECS/xroad-catalog-lister.spec
